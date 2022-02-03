d3da49fd234d3bdfe5667b778f13aa83cde35925
MihailBelyuk <Tamerlan123Sadist123>
2022-02-03 12:45:07 +0300

    Project

--- src/main/java/com/belyuk/first_project/validator/impl/ValidatorImpl.java
+++ src/main/java/com/belyuk/first_project/validator/impl/ValidatorImpl.java
@@ -2,6 +2,7 @@ package com.belyuk.first_project.validator.impl;
 
 import com.belyuk.first_project.validator.Validator;
 import java.io.File;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,17 +19,11 @@ public class ValidatorImpl implements Validator {
   @Override
   public boolean validateFileInfo(String filePath) {
     boolean isValid = false;
-    if(filePath == null){
-      isValid = false;
-    }
-    File file = new File(filePath);
-    if (file == null) {
-      isValid = false;
-    } else {
+    if (filePath != null) {
+      File file = new File(filePath);
       if (file.exists() && file.length() > 0) {
         isValid = true;
-      } else {
-        isValid = false;
+        LOGGER.log(Level.INFO,"File path is valid.");
       }
     }
     return isValid;
